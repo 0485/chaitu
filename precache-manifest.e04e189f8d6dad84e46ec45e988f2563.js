4b3fa27cc64ace6f4ad8583d97d6fd3016d4ed47
048 <rp814043@gmail.com>
2020-03-12 15:40:19 +0530

    Updates

---
+++ precache-manifest.e04e189f8d6dad84e46ec45e988f2563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e5bd36caeafae7676a0a83d4b434b2",
+    "url": "/chaitu/index.html"
+  },
+  {
+    "revision": "3c8162abb28122e01d36",
+    "url": "/chaitu/static/css/main.12730ed6.chunk.css"
+  },
+  {
+    "revision": "04f481582293fb6f7410",
+    "url": "/chaitu/static/js/2.05f3e1a1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/chaitu/static/js/2.05f3e1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8162abb28122e01d36",
+    "url": "/chaitu/static/js/main.952c66da.chunk.js"
+  },
+  {
+    "revision": "2b93fa30b40ed022f22b",
+    "url": "/chaitu/static/js/runtime-main.a9afe65a.js"
+  },
+  {
+    "revision": "3d5dc110b20d19dfd6ad5bc565d2ff7f",
+    "url": "/chaitu/static/media/avatar.3d5dc110.svg"
+  }
+]);
